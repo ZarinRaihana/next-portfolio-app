b012568141d39655c77500dc8c3018575245905a
ZarinRaihana <zarinraihana09@gmail.com>
2021-12-05 20:40:45 +0600

    text

--- components/About.js
+++ components/About.js
@@ -8,7 +8,7 @@ const About = () => {
                 <div className={classes.aboutArea} >
                     <h2 className={classes.title}>About me</h2>
                     <p className={classes.text}>
-                    My name is Zarin Raihana. I&apos;m a fresh graduate looking for job opportunities, interested working in front-end development. I have completed my Bachelor&apos;s degree in Computer Science & Engineering at Ranada Prasad Shaha University. Love to watch movies and tv series.
+                    My name is Zarin Raihana. I&apos;m a fresh graduate looking for job opportunities, interested working in JavaScript development. I have completed my Bachelor&apos;s degree in Computer Science & Engineering from Ranada Prasad Shaha University. In my free time, i love to watch movies and tv series.
                     </p>
                 </div>
             </section>
